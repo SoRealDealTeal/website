2bf78e0c95eb19b3b24c17832dec961c8c610f79
SoRealDealTeal <124551956+SoRealDealTeal@users.noreply.github.com>
2023-09-20 21:51:05 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask, request, jsonify
+import openai
+
+
+# Initialize Flask app
+app = Flask(__name__)
+
+
+# Set up OpenAI API key
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_message = request.json.get('message')
+    response = openai.Completion.create(
+      model="text-davinci-002",
+      prompt=user_message,
+      max_tokens=150
+    )
+    return jsonify(response=response.choices[0].text.strip())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
